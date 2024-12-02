e89cad20be1b80e143ac1e3834386757579a5a41
Chris Pick <chris@chrispick.com>
2024-12-02 15:09:06 -0500

    Non-booting work in progress

--- flake.nix
+++ flake.nix
@@ -18,6 +18,47 @@
   in {
     packages."${linuxSystem}".default = nixos-generators.nixosGenerate {
       format = "raw-efi";
+      modules = [
+        {
+          imports = [
+            (nixpkgs + "/nixos/modules/profiles/qemu-guest.nix")
+          ];
+          # FIXME: ?
+          # nix.registry.nixpkgs.flake = nixpkgs;
+          # virtualisation.diskSize = 10 * 1024;
+          # virtualisation.rosetta.enable = true;
+
+          # boot.loader = {
+          #   systemd-boot.enable = true; 
+          #   efi.canTouchEfiVariables = true;
+          # };
+          boot.loader.grub = {
+            device = "nodev";
+            efiSupport = true;
+            efiInstallAsRemovable = true;
+          };
+
+          services.openssh = {
+            enable = true;
+            settings.PermitRootLogin = "yes";
+          };
+
+          users.users.root.password = "nixos"; # FIXME:
+
+          fileSystems."/boot" = {
+            device = "/dev/disk/by-label/ESP"; # /dev/vda1
+            fsType = "vfat";
+          };
+
+          fileSystems."/" = {
+            device = "/dev/disk/by-label/nixos";
+            autoResize = true;
+            fsType = "ext4";
+            options = ["noatime" "nodiratime" "discard"];
+          };
+
+        }
+      ];
       system = linuxSystem;
     };
 
