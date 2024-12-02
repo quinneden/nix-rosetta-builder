48ca8745a6396c4ddc9af46c4364ff659d08bca3
Chris Pick <chris@chrispick.com>
2024-12-02 10:16:58 -0500

    Generate raw-efi image as default package

--- flake.nix
+++ flake.nix
@@ -2,17 +2,27 @@
   description = "Lima-based, Rosetta 2-enabled, macOS (Darwin) builder";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages."${system}";
+  outputs = { self, nixos-generators, nixpkgs }:
+  let
+    system = "aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages."${system}";
+    linuxSystem = builtins.replaceStrings [ "darwin" ] [ "linux" ] system;
 
-    in {
-      devShells."${system}".default = pkgs.mkShell { packages = [
-        pkgs.lima
-      ]; };
+  in {
+    packages."${linuxSystem}".default = nixos-generators.nixosGenerate {
+      format = "raw-efi";
+      system = linuxSystem;
     };
+
+    devShells."${system}".default = pkgs.mkShell { packages = [
+      pkgs.lima
+    ]; };
+  };
 }
