44d346631c9aceb4ee3cfd6aabb3f2d6da0cf193
Chris Pick <chris@chrispick.com>
2024-12-24 17:08:51 -0500

    Add `config.rosetta-builder.enable`
    Default to `true`, but provide as a way to disable the module.

--- module.nix
+++ module.nix
@@ -2,7 +2,7 @@
 { linuxSystem
 , package
 }:
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 let
   inherit (import ./constants.nix)
     name
@@ -17,6 +17,7 @@ let
 
     debug
     ;
+  cfg = config.nix-rosetta-builder;
   cores = 8;
   daemonName = "${name}d";
   daemonSocketName = "Listener";
@@ -73,173 +74,178 @@ let
   };
 
 in {
-  environment.etc."ssh/ssh_config.d/100-${sshHost}.conf".text = ''
-    Host "${sshHost}"
-      GlobalKnownHostsFile "${workingDirPath}/${sshGlobalKnownHostsFileName}"
-      Hostname localhost
-      HostKeyAlias "${sshHostKeyAlias}"
-      Port "${toString port}"
-      StrictHostKeyChecking yes
-      User "${linuxUser}"
-      IdentityFile "${workingDirPath}/${sshUserPrivateKeyFileName}"
-  '';
+  options.nix-rosetta-builder = {
+    enable = (lib.mkEnableOption "Nix Rosetta Linux builder") // { default = true; };
+  };
 
-  launchd.daemons."${daemonName}" = {
-    path = [
-      pkgs.coreutils
-      pkgs.gnugrep
-      (pkgs.lima.overrideAttrs (old: {
-        src = pkgs.fetchFromGitHub {
-          owner = "cpick";
-          repo = "lima";
-          rev = "afbfdfb8dd5fa370547b7fc64a16ce2a354b1ff0";
-          hash = "sha256-tCildZJp6ls+WxRAbkoeLRb4WdroBYn/gvE5Vb8Hm5A=";
-        };
-
-        vendorHash = "sha256-I84971WovhJL/VO/Ycu12qa9lDL3F9USxlt9rXcsnTU=";
-      }))
-      pkgs.openssh
-
-      # Lima calls `sw_vers` which is not packaged in Nix:
-      # https://github.com/lima-vm/lima/blob/0e931107cadbcb6dbc7bbb25626f66cdbca1f040/pkg/osutil/osversion_darwin.go#L13
-      # If the call fails it will not use the Virtualization framework bakend (by default? among
-      # other things?).
-      "/usr/bin"
-    ];
-
-    script =
-    let
-      darwinUserSh = lib.escapeShellArg darwinUser;
-      linuxHostNameSh = lib.escapeShellArg linuxHostName;
-      linuxSshdKeysDirNameSh = lib.escapeShellArg linuxSshdKeysDirName;
-      sshGlobalKnownHostsFileNameSh = lib.escapeShellArg sshGlobalKnownHostsFileName;
-      sshHostKeyAliasSh = lib.escapeShellArg sshHostKeyAlias;
-      sshHostPrivateKeyFileNameSh = lib.escapeShellArg sshHostPrivateKeyFileName;
-      sshHostPublicKeyFileNameSh = lib.escapeShellArg sshHostPublicKeyFileName;
-      sshKeyTypeSh = lib.escapeShellArg sshKeyType;
-      sshUserPrivateKeyFileNameSh = lib.escapeShellArg sshUserPrivateKeyFileName;
-      sshUserPublicKeyFileNameSh = lib.escapeShellArg sshUserPublicKeyFileName;
-      vmNameSh = lib.escapeShellArg "${name}-vm";
-      vmYamlSh = lib.escapeShellArg vmYaml;
-
-    in ''
-      set -e
-      set -u
-
-      umask 'g-w,o='
-      chmod 'g-w,o=' .
-
-      # must be idempotent in the face of partial failues
-      limactl list -q 2>'/dev/null' | grep -q ${vmNameSh} || {
-        yes | ssh-keygen \
-          -C ${darwinUserSh}@darwin -f ${sshUserPrivateKeyFileNameSh} -N "" -t ${sshKeyTypeSh}
-        yes | ssh-keygen \
-          -C root@${linuxHostNameSh} -f ${sshHostPrivateKeyFileNameSh} -N "" -t ${sshKeyTypeSh}
-
-        mkdir -p ${linuxSshdKeysDirNameSh}
-        mv \
-          ${sshUserPublicKeyFileNameSh} ${sshHostPrivateKeyFileNameSh} ${linuxSshdKeysDirNameSh}
-
-        echo ${sshHostKeyAliasSh} "$(cat ${sshHostPublicKeyFileNameSh})" \
-        >${sshGlobalKnownHostsFileNameSh}
-
-        # must be last so `limactl list` only now succeeds
-        limactl create --name=${vmNameSh} ${vmYamlSh}
-      }
-
-      exec limactl start ${lib.optionalString debug "--debug"} --foreground ${vmNameSh}
+  config = lib.mkIf cfg.enable {
+    environment.etc."ssh/ssh_config.d/100-${sshHost}.conf".text = ''
+      Host "${sshHost}"
+        GlobalKnownHostsFile "${workingDirPath}/${sshGlobalKnownHostsFileName}"
+        Hostname localhost
+        HostKeyAlias "${sshHostKeyAlias}"
+        Port "${toString port}"
+        StrictHostKeyChecking yes
+        User "${linuxUser}"
+        IdentityFile "${workingDirPath}/${sshUserPrivateKeyFileName}"
     '';
 
-    serviceConfig = {
-      KeepAlive = !onDemand;
+    launchd.daemons."${daemonName}" = {
+      path = [
+        pkgs.coreutils
+        pkgs.gnugrep
+        (pkgs.lima.overrideAttrs (old: {
+          src = pkgs.fetchFromGitHub {
+            owner = "cpick";
+            repo = "lima";
+            rev = "afbfdfb8dd5fa370547b7fc64a16ce2a354b1ff0";
+            hash = "sha256-tCildZJp6ls+WxRAbkoeLRb4WdroBYn/gvE5Vb8Hm5A=";
+          };
 
-      Sockets."${daemonSocketName}" = lib.optionalAttrs onDemand {
-        SockFamily = "IPv4";
-        SockNodeName = "localhost";
-        SockServiceName = toString port;
+          vendorHash = "sha256-I84971WovhJL/VO/Ycu12qa9lDL3F9USxlt9rXcsnTU=";
+        }))
+        pkgs.openssh
+
+        # Lima calls `sw_vers` which is not packaged in Nix:
+        # https://github.com/lima-vm/lima/blob/0e931107cadbcb6dbc7bbb25626f66cdbca1f040/pkg/osutil/osversion_darwin.go#L13
+        # If the call fails it will not use the Virtualization framework bakend (by default? among
+        # other things?).
+        "/usr/bin"
+      ];
+
+      script =
+      let
+        darwinUserSh = lib.escapeShellArg darwinUser;
+        linuxHostNameSh = lib.escapeShellArg linuxHostName;
+        linuxSshdKeysDirNameSh = lib.escapeShellArg linuxSshdKeysDirName;
+        sshGlobalKnownHostsFileNameSh = lib.escapeShellArg sshGlobalKnownHostsFileName;
+        sshHostKeyAliasSh = lib.escapeShellArg sshHostKeyAlias;
+        sshHostPrivateKeyFileNameSh = lib.escapeShellArg sshHostPrivateKeyFileName;
+        sshHostPublicKeyFileNameSh = lib.escapeShellArg sshHostPublicKeyFileName;
+        sshKeyTypeSh = lib.escapeShellArg sshKeyType;
+        sshUserPrivateKeyFileNameSh = lib.escapeShellArg sshUserPrivateKeyFileName;
+        sshUserPublicKeyFileNameSh = lib.escapeShellArg sshUserPublicKeyFileName;
+        vmNameSh = lib.escapeShellArg "${name}-vm";
+        vmYamlSh = lib.escapeShellArg vmYaml;
+
+      in ''
+        set -e
+        set -u
+
+        umask 'g-w,o='
+        chmod 'g-w,o=' .
+
+        # must be idempotent in the face of partial failues
+        limactl list -q 2>'/dev/null' | grep -q ${vmNameSh} || {
+          yes | ssh-keygen \
+            -C ${darwinUserSh}@darwin -f ${sshUserPrivateKeyFileNameSh} -N "" -t ${sshKeyTypeSh}
+          yes | ssh-keygen \
+            -C root@${linuxHostNameSh} -f ${sshHostPrivateKeyFileNameSh} -N "" -t ${sshKeyTypeSh}
+
+          mkdir -p ${linuxSshdKeysDirNameSh}
+          mv \
+            ${sshUserPublicKeyFileNameSh} ${sshHostPrivateKeyFileNameSh} ${linuxSshdKeysDirNameSh}
+
+          echo ${sshHostKeyAliasSh} "$(cat ${sshHostPublicKeyFileNameSh})" \
+          >${sshGlobalKnownHostsFileNameSh}
+
+          # must be last so `limactl list` only now succeeds
+          limactl create --name=${vmNameSh} ${vmYamlSh}
+        }
+
+        exec limactl start ${lib.optionalString debug "--debug"} --foreground ${vmNameSh}
+      '';
+
+      serviceConfig = {
+        KeepAlive = !onDemand;
+
+        Sockets."${daemonSocketName}" = lib.optionalAttrs onDemand {
+          SockFamily = "IPv4";
+          SockNodeName = "localhost";
+          SockServiceName = toString port;
+        };
+
+        UserName = darwinUser;
+        WorkingDirectory = workingDirPath;
+      } // lib.optionalAttrs debug {
+        StandardErrorPath = "/tmp/${daemonName}.err.log";
+        StandardOutPath = "/tmp/${daemonName}.out.log";
       };
-
-      UserName = darwinUser;
-      WorkingDirectory = workingDirPath;
-    } // lib.optionalAttrs debug {
-      StandardErrorPath = "/tmp/${daemonName}.err.log";
-      StandardOutPath = "/tmp/${daemonName}.out.log";
     };
+
+    nix = {
+      buildMachines = [{
+        hostName = sshHost;
+        maxJobs = cores;
+        protocol = "ssh-ng";
+        supportedFeatures = [ "benchmark" "big-parallel" "kvm" ];
+        systems = [ linuxSystem "x86_64-linux" ];
+      }];
+
+      distributedBuilds = true;
+      settings.builders-use-substitutes = true;
+    };
+
+    # `users.users` cannot create a service account and cannot create an empty home directory so do
+    # it manually in an activation script.  This `extraActivation` was chosen in particiular because
+    # it's one of the system level (as opposed to user level) ones that's been set aside for
+    # customization:
+    # https://github.com/LnL7/nix-darwin/blob/a35b08d09efda83625bef267eb24347b446c80b8/modules/system/activation-scripts.nix#L121-L125
+    # And of those, it's the one that's executed latest but still before
+    # `activationScripts.launchd` which needs the group, user, and directory in place:
+    # https://github.com/LnL7/nix-darwin/blob/a35b08d09efda83625bef267eb24347b446c80b8/modules/system/activation-scripts.nix#L58-L66
+    system.activationScripts.extraActivation.text =
+    let
+      gidSh = lib.escapeShellArg (toString darwinGid);
+      groupSh = lib.escapeShellArg darwinGroup;
+      groupPathSh = lib.escapeShellArg "/Groups/${darwinGroup}";
+
+      uidSh = lib.escapeShellArg (toString darwinUid);
+      userSh = lib.escapeShellArg darwinUser;
+      userPathSh = lib.escapeShellArg "/Users/${darwinUser}";
+
+      workingDirPathSh = lib.escapeShellArg workingDirPath;
+
+    # apply "after" to work cooperatively with any other modules using this activation script
+    in lib.mkAfter ''
+      printf >&2 'setting up group %s...\n' ${groupSh}
+
+      if ! primaryGroupId="$(dscl . -read ${groupPathSh} 'PrimaryGroupID' 2>'/dev/null')" ; then
+        printf >&2 'creating group %s...\n' ${groupSh}
+        dscl . -create ${groupPathSh} 'PrimaryGroupID' ${gidSh}
+      elif [[ "$primaryGroupId" != *\ ${gidSh} ]] ; then
+        printf >&2 \
+          '\e[1;31merror: existing group: %s has unexpected %s\e[0m\n' \
+          ${groupSh} \
+          "$primaryGroupId"
+        exit 1
+      fi
+      unset 'primaryGroupId'
+
+
+      printf >&2 'setting up user %s...\n' ${userSh}
+
+      if ! uid="$(id -u ${userSh} 2>'/dev/null')" ; then
+        printf >&2 'creating user %s...\n' ${userSh}
+        dscl . -create ${userPathSh}
+        dscl . -create ${userPathSh} 'PrimaryGroupID' ${gidSh}
+        dscl . -create ${userPathSh} 'NFSHomeDirectory' ${workingDirPathSh}
+        dscl . -create ${userPathSh} 'UserShell' '/usr/bin/false'
+        dscl . -create ${userPathSh} 'IsHidden' 1
+        dscl . -create ${userPathSh} 'UniqueID' ${uidSh} # must be last so `id` only now succeeds
+      elif [ "$uid" -ne ${uidSh} ] ; then
+        printf >&2 \
+          '\e[1;31merror: existing user: %s has unexpected UID: %s\e[0m\n' \
+          ${userSh} \
+          "$uid"
+        exit 1
+      fi
+      unset 'uid'
+
+
+      printf >&2 'setting up working directory %s...\n' ${workingDirPathSh}
+      mkdir -p ${workingDirPathSh}
+      chown ${userSh}:${groupSh} ${workingDirPathSh}
+    '';
   };
-
-  nix = {
-    buildMachines = [{
-      hostName = sshHost;
-      maxJobs = cores;
-      protocol = "ssh-ng";
-      supportedFeatures = [ "benchmark" "big-parallel" "kvm" ];
-      systems = [ linuxSystem "x86_64-linux" ];
-    }];
-
-    distributedBuilds = true;
-    settings.builders-use-substitutes = true;
-  };
-
-  # `users.users` cannot create a service account and cannot create an empty home directory so do it
-  # manually in an activation script.  This `extraActivation` was chosen in particiular because
-  # it's one of the system level (as opposed to user level) ones that's been set aside for
-  # customization:
-  # https://github.com/LnL7/nix-darwin/blob/a35b08d09efda83625bef267eb24347b446c80b8/modules/system/activation-scripts.nix#L121-L125
-  # And of those, it's the one that's executed latest but still before
-  # `activationScripts.launchd` which needs the group, user, and directory in place:
-  # https://github.com/LnL7/nix-darwin/blob/a35b08d09efda83625bef267eb24347b446c80b8/modules/system/activation-scripts.nix#L58-L66
-  system.activationScripts.extraActivation.text =
-  let
-    gidSh = lib.escapeShellArg (toString darwinGid);
-    groupSh = lib.escapeShellArg darwinGroup;
-    groupPathSh = lib.escapeShellArg "/Groups/${darwinGroup}";
-
-    uidSh = lib.escapeShellArg (toString darwinUid);
-    userSh = lib.escapeShellArg darwinUser;
-    userPathSh = lib.escapeShellArg "/Users/${darwinUser}";
-
-    workingDirPathSh = lib.escapeShellArg workingDirPath;
-
-  # apply "after" to work cooperatively with any other modules using this activation script
-  in lib.mkAfter ''
-    printf >&2 'setting up group %s...\n' ${groupSh}
-
-    if ! primaryGroupId="$(dscl . -read ${groupPathSh} 'PrimaryGroupID' 2>'/dev/null')" ; then
-      printf >&2 'creating group %s...\n' ${groupSh}
-      dscl . -create ${groupPathSh} 'PrimaryGroupID' ${gidSh}
-    elif [[ "$primaryGroupId" != *\ ${gidSh} ]] ; then
-      printf >&2 \
-        '\e[1;31merror: existing group: %s has unexpected %s\e[0m\n' \
-        ${groupSh} \
-        "$primaryGroupId"
-      exit 1
-    fi
-    unset 'primaryGroupId'
-
-
-    printf >&2 'setting up user %s...\n' ${userSh}
-
-    if ! uid="$(id -u ${userSh} 2>'/dev/null')" ; then
-      printf >&2 'creating user %s...\n' ${userSh}
-      dscl . -create ${userPathSh}
-      dscl . -create ${userPathSh} 'PrimaryGroupID' ${gidSh}
-      dscl . -create ${userPathSh} 'NFSHomeDirectory' ${workingDirPathSh}
-      dscl . -create ${userPathSh} 'UserShell' '/usr/bin/false'
-      dscl . -create ${userPathSh} 'IsHidden' 1
-      dscl . -create ${userPathSh} 'UniqueID' ${uidSh} # must be last so `id` only now succeeds
-    elif [ "$uid" -ne ${uidSh} ] ; then
-      printf >&2 \
-        '\e[1;31merror: existing user: %s has unexpected UID: %s\e[0m\n' \
-        ${userSh} \
-        "$uid"
-      exit 1
-    fi
-    unset 'uid'
-
-
-    printf >&2 'setting up working directory %s...\n' ${workingDirPathSh}
-    mkdir -p ${workingDirPathSh}
-    chown ${userSh}:${groupSh} ${workingDirPathSh}
-  '';
-
 }
