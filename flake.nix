be6728490f78594dc53684fee62014db1ed664a5
quinneden <quinnyxboy@gmail.com>
2025-01-28 14:07:54 -0800

    opinionated changes

--- flake.nix
+++ flake.nix
@@ -3,38 +3,52 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixos-generators, nixpkgs }:
-  let
-    darwinSystem = "aarch64-darwin";
-    linuxSystem = builtins.replaceStrings [ "darwin" ] [ "linux" ] darwinSystem;
-    lib = nixpkgs.lib;
-
-  in {
-    packages."${linuxSystem}" =
+  outputs =
+    {
+      self,
+      nixos-generators,
+      nixpkgs,
+    }:
     let
-      arguments = { inherit linuxSystem nixos-generators nixpkgs; };
-      pkgs = nixpkgs.legacyPackages."${linuxSystem}";
-    in {
-      default = pkgs.callPackage ./package.nix (arguments // { onDemand = false; });
-      on-demand = pkgs.callPackage ./package.nix (arguments // { onDemand = true; });
-    };
+      darwinSystem = "aarch64-darwin";
+      linuxSystem = builtins.replaceStrings [ "darwin" ] [ "linux" ] darwinSystem;
+    in
+    {
+      packages."${linuxSystem}" =
+        let
+          arguments = { inherit linuxSystem nixos-generators nixpkgs; };
+          pkgs = nixpkgs.legacyPackages."${linuxSystem}";
+        in
+        {
+          default = pkgs.callPackage ./package.nix (arguments // { onDemand = false; });
+          on-demand = pkgs.callPackage ./package.nix (arguments // { onDemand = true; });
+        };
 
-    devShells."${darwinSystem}".default =
-    let
-      pkgs = nixpkgs.legacyPackages."${darwinSystem}";
-    in pkgs.mkShell {
-      packages = [ pkgs.lima ];
-    };
+      devShells."${darwinSystem}".default =
+        let
+          pkgs = nixpkgs.legacyPackages."${darwinSystem}";
+        in
+        pkgs.mkShell {
+          packages = [ pkgs.lima ];
+        };
 
-    darwinModules.default = import ./module.nix {
-      images = self.packages."${linuxSystem}";
-      inherit linuxSystem;
+      darwinModules.default = import ./module.nix {
+        images = self.packages."${linuxSystem}";
+        inherit linuxSystem;
+      };
+
+      formatter."${darwinSystem}" = nixpkgs.legacyPackages."${darwinSystem}".nixfmt-rfc-style;
     };
-  };
 }
--- package.nix
+++ package.nix
@@ -1,196 +1,215 @@
 # dependencies
-{ nixos-generators
-, nixpkgs
+{
+  nixos-generators,
+  nixpkgs,
 
-# pkgs
-, lib
-, mount
-, umount
+  # pkgs
+  lib,
+  mount,
+  umount,
 
-# configuration
-, linuxSystem
-, onDemand # enable launchd socket activation
+  # configuration
+  linuxSystem,
+  onDemand, # enable launchd socket activation
 }:
 nixos-generators.nixosGenerate (
-let
-  inherit (import ./constants.nix)
-    linuxHostName
-    linuxUser
+  let
+    inherit (import ./constants.nix)
+      linuxHostName
+      linuxUser
 
-    sshHostPrivateKeyFileName
-    sshUserPublicKeyFileName
+      sshHostPrivateKeyFileName
+      sshUserPublicKeyFileName
 
-    debug
-    ;
-  imageFormat = "qcow-efi"; # must match `vmYaml.images.location`s extension
+      debug
+      ;
+    imageFormat = "qcow-efi"; # must match `vmYaml.images.location`s extension
 
-  sshdKeys = "sshd-keys";
-  sshDirPath = "/etc/ssh";
-  sshHostPrivateKeyFilePath = "${sshDirPath}/${sshHostPrivateKeyFileName}";
+    sshdKeys = "sshd-keys";
+    sshDirPath = "/etc/ssh";
+    sshHostPrivateKeyFilePath = "${sshDirPath}/${sshHostPrivateKeyFileName}";
 
-in {
-  format = imageFormat;
+  in
+  {
+    format = imageFormat;
 
-  modules = [ {
-    boot = {
-      kernelParams = [ "console=tty0" ];
+    modules = [
+      {
+        boot = {
+          kernelParams = [ "console=tty0" ];
 
-      loader = {
-        efi.canTouchEfiVariables = true;
-        systemd-boot.enable = true;
-      };
-    };
-
-    documentation.enable = false;
-
-    fileSystems = {
-      "/".options = [ "discard" "noatime" ];
-      "/boot".options = [ "discard" "noatime" "umask=0077" ];
-    };
-
-    networking.hostName = linuxHostName;
-
-    nix = {
-      channel.enable = false;
-      registry.nixpkgs.flake = nixpkgs;
-
-      settings = {
-        auto-optimise-store = true;
-        experimental-features = [ "flakes" "nix-command" ];
-        min-free = "5G";
-        max-free = "7G";
-        trusted-users = [ linuxUser ];
-      };
-    };
-
-    security = {
-      sudo = {
-        enable = debug;
-        wheelNeedsPassword = !debug;
-      };
-    };
-
-    services = {
-      getty = lib.optionalAttrs debug { autologinUser = linuxUser; };
-
-      logind = lib.optionalAttrs onDemand {
-        extraConfig = ''
-          IdleAction=poweroff
-          IdleActionSec=3h
-        '';
-      };
-
-      openssh = {
-        enable = true;
-        hostKeys = []; # disable automatic host key generation
-
-        settings = {
-          HostKey = sshHostPrivateKeyFilePath;
-          PasswordAuthentication = false;
+          loader = {
+            efi.canTouchEfiVariables = true;
+            systemd-boot.enable = true;
+          };
         };
-      };
-    };
 
-    system = {
-      disableInstallerTools = true;
-      stateVersion = "24.05";
-    };
+        documentation.enable = false;
 
-    # macOS' Virtualization framework's virtiofs implementation will grant any guest user access
-    # to mounted files; they always appear to be owned by the effective UID and so access cannot
-    # be restricted.
-    # To protect the guest's SSH host key, the VM is configured to prevent any logins (via
-    # console, SSH, etc) by default.  This service then runs before sshd, mounts virtiofs,
-    # copies the keys to local files (with appropriate ownership and permissions), and unmounts
-    # the filesystem before allowing SSH to start.
-    # Once SSH has been allowed to start (and given the guest user a chance to log in), the
-    # virtiofs must never be mounted again (as the user could have left some process active to
-    # read its secrets).  This is prevented by `unitconfig.ConditionPathExists` below.
-    systemd.services."${sshdKeys}" =
-    let
-      # Lima labels its virtiofs folder mounts counting up:
-      # https://github.com/lima-vm/lima/blob/0e931107cadbcb6dbc7bbb25626f66cdbca1f040/pkg/vz/vm_darwin.go#L568
-      # So this suffix must match `vmYaml.mounts.location`s order:
-      sshdKeysVirtiofsTag = "mount0";
+        fileSystems = {
+          "/".options = [
+            "discard"
+            "noatime"
+          ];
+          "/boot".options = [
+            "discard"
+            "noatime"
+            "umask=0077"
+          ];
+        };
 
-      sshdKeysDirPath = "/var/${sshdKeys}";
-      sshAuthorizedKeysUserFilePath = "${sshDirPath}/authorized_keys.d/${linuxUser}";
-      sshdService = "sshd.service";
+        networking.hostName = linuxHostName;
 
-    in {
-      before = [ sshdService ];
-      description = "Install sshd's host and authorized keys";
-      enableStrictShellChecks = true;
-      path = [ mount umount ];
-      requiredBy = [ sshdService ];
+        nix = {
+          channel.enable = false;
+          registry.nixpkgs.flake = nixpkgs;
 
-      script =
-      let
-        sshAuthorizedKeysUserFilePathSh = lib.escapeShellArg sshAuthorizedKeysUserFilePath;
-        sshAuthorizedKeysUserTmpFilePathSh =
-          lib.escapeShellArg "${sshAuthorizedKeysUserFilePath}.tmp";
-        sshHostPrivateKeyFileNameSh = lib.escapeShellArg sshHostPrivateKeyFileName;
-        sshHostPrivateKeyFilePathSh = lib.escapeShellArg sshHostPrivateKeyFilePath;
-        sshUserPublicKeyFileNameSh = lib.escapeShellArg sshUserPublicKeyFileName;
-        sshdKeysDirPathSh = lib.escapeShellArg sshdKeysDirPath;
-        sshdKeysVirtiofsTagSh = lib.escapeShellArg sshdKeysVirtiofsTag;
+          settings = {
+            auto-optimise-store = true;
+            experimental-features = [
+              "flakes"
+              "nix-command"
+            ];
+            min-free = "5G";
+            max-free = "7G";
+            trusted-users = [ linuxUser ];
+          };
+        };
 
-      in ''
-        # must be idempotent in the face of partial failues
+        security = {
+          sudo = {
+            enable = debug;
+            wheelNeedsPassword = !debug;
+          };
+        };
 
-        mkdir -p ${sshdKeysDirPathSh}
-        mount \
-          -t 'virtiofs' \
-          -o 'nodev,noexec,nosuid,ro' \
-          ${sshdKeysVirtiofsTagSh} \
-          ${sshdKeysDirPathSh}
+        services = {
+          getty = lib.optionalAttrs debug { autologinUser = linuxUser; };
 
-        mkdir -p "$(dirname ${sshHostPrivateKeyFilePathSh})"
-        (
-          umask 'go='
-          cp ${sshdKeysDirPathSh}/${sshHostPrivateKeyFileNameSh} ${sshHostPrivateKeyFilePathSh}
-        )
+          logind = lib.optionalAttrs onDemand {
+            extraConfig = ''
+              IdleAction=poweroff
+              IdleActionSec=3h
+            '';
+          };
 
-        mkdir -p "$(dirname ${sshAuthorizedKeysUserTmpFilePathSh})"
-        cp \
-          ${sshdKeysDirPathSh}/${sshUserPublicKeyFileNameSh} \
-          ${sshAuthorizedKeysUserTmpFilePathSh}
-        chmod 'a+r' ${sshAuthorizedKeysUserTmpFilePathSh}
+          openssh = {
+            enable = true;
+            hostKeys = [ ]; # disable automatic host key generation
 
-        umount ${sshdKeysDirPathSh}
-        rmdir ${sshdKeysDirPathSh}
+            settings = {
+              HostKey = sshHostPrivateKeyFilePath;
+              PasswordAuthentication = false;
+            };
+          };
+        };
 
-        # must be last so only now `unitConfig.ConditionPathExists` triggers
-        mv ${sshAuthorizedKeysUserTmpFilePathSh} ${sshAuthorizedKeysUserFilePathSh}
-      '';
+        system = {
+          disableInstallerTools = true;
+          stateVersion = "24.05";
+        };
 
-      serviceConfig.Type = "oneshot";
+        # macOS' Virtualization framework's virtiofs implementation will grant any guest user access
+        # to mounted files; they always appear to be owned by the effective UID and so access cannot
+        # be restricted.
+        # To protect the guest's SSH host key, the VM is configured to prevent any logins (via
+        # console, SSH, etc) by default.  This service then runs before sshd, mounts virtiofs,
+        # copies the keys to local files (with appropriate ownership and permissions), and unmounts
+        # the filesystem before allowing SSH to start.
+        # Once SSH has been allowed to start (and given the guest user a chance to log in), the
+        # virtiofs must never be mounted again (as the user could have left some process active to
+        # read its secrets).  This is prevented by `unitconfig.ConditionPathExists` below.
+        systemd.services."${sshdKeys}" =
+          let
+            # Lima labels its virtiofs folder mounts counting up:
+            # https://github.com/lima-vm/lima/blob/0e931107cadbcb6dbc7bbb25626f66cdbca1f040/pkg/vz/vm_darwin.go#L568
+            # So this suffix must match `vmYaml.mounts.location`s order:
+            sshdKeysVirtiofsTag = "mount0";
 
-      # see comments on this service and in its `script`
-      unitConfig.ConditionPathExists = "!${sshAuthorizedKeysUserFilePath}";
-    };
+            sshdKeysDirPath = "/var/${sshdKeys}";
+            sshAuthorizedKeysUserFilePath = "${sshDirPath}/authorized_keys.d/${linuxUser}";
+            sshdService = "sshd.service";
 
-    users = {
-      # console and (initial) SSH logins are purposely disabled
-      # see: `systemd.services."${sshdKeys}"`
-      allowNoPasswordLogin = true;
+          in
+          {
+            before = [ sshdService ];
+            description = "Install sshd's host and authorized keys";
+            enableStrictShellChecks = true;
+            path = [
+              mount
+              umount
+            ];
+            requiredBy = [ sshdService ];
 
-      mutableUsers = false;
+            script =
+              let
+                sshAuthorizedKeysUserFilePathSh = lib.escapeShellArg sshAuthorizedKeysUserFilePath;
+                sshAuthorizedKeysUserTmpFilePathSh = lib.escapeShellArg "${sshAuthorizedKeysUserFilePath}.tmp";
+                sshHostPrivateKeyFileNameSh = lib.escapeShellArg sshHostPrivateKeyFileName;
+                sshHostPrivateKeyFilePathSh = lib.escapeShellArg sshHostPrivateKeyFilePath;
+                sshUserPublicKeyFileNameSh = lib.escapeShellArg sshUserPublicKeyFileName;
+                sshdKeysDirPathSh = lib.escapeShellArg sshdKeysDirPath;
+                sshdKeysVirtiofsTagSh = lib.escapeShellArg sshdKeysVirtiofsTag;
 
-      users."${linuxUser}" = {
-        isNormalUser = true;
-        extraGroups = lib.optionals debug [ "wheel" ];
-      };
-    };
+              in
+              ''
+                # must be idempotent in the face of partial failues
 
-    virtualisation.rosetta = {
-      enable = true;
+                mkdir -p ${sshdKeysDirPathSh}
+                mount \
+                  -t 'virtiofs' \
+                  -o 'nodev,noexec,nosuid,ro' \
+                  ${sshdKeysVirtiofsTagSh} \
+                  ${sshdKeysDirPathSh}
 
-      # Lima's virtiofs label for rosetta:
-      # https://github.com/lima-vm/lima/blob/0e931107cadbcb6dbc7bbb25626f66cdbca1f040/pkg/vz/rosetta_directory_share_arm64.go#L15
-      mountTag = "vz-rosetta";
-    };
-  } ];
+                mkdir -p "$(dirname ${sshHostPrivateKeyFilePathSh})"
+                (
+                  umask 'go='
+                  cp ${sshdKeysDirPathSh}/${sshHostPrivateKeyFileNameSh} ${sshHostPrivateKeyFilePathSh}
+                )
 
-  system = linuxSystem;
-})
+                mkdir -p "$(dirname ${sshAuthorizedKeysUserTmpFilePathSh})"
+                cp \
+                  ${sshdKeysDirPathSh}/${sshUserPublicKeyFileNameSh} \
+                  ${sshAuthorizedKeysUserTmpFilePathSh}
+                chmod 'a+r' ${sshAuthorizedKeysUserTmpFilePathSh}
+
+                umount ${sshdKeysDirPathSh}
+                rmdir ${sshdKeysDirPathSh}
+
+                # must be last so only now `unitConfig.ConditionPathExists` triggers
+                mv ${sshAuthorizedKeysUserTmpFilePathSh} ${sshAuthorizedKeysUserFilePathSh}
+              '';
+
+            serviceConfig.Type = "oneshot";
+
+            # see comments on this service and in its `script`
+            unitConfig.ConditionPathExists = "!${sshAuthorizedKeysUserFilePath}";
+          };
+
+        users = {
+          # console and (initial) SSH logins are purposely disabled
+          # see: `systemd.services."${sshdKeys}"`
+          allowNoPasswordLogin = true;
+
+          mutableUsers = false;
+
+          users."${linuxUser}" = {
+            isNormalUser = true;
+            extraGroups = lib.optionals debug [ "wheel" ];
+          };
+        };
+
+        virtualisation.rosetta = {
+          enable = true;
+
+          # Lima's virtiofs label for rosetta:
+          # https://github.com/lima-vm/lima/blob/0e931107cadbcb6dbc7bbb25626f66cdbca1f040/pkg/vz/rosetta_directory_share_arm64.go#L15
+          mountTag = "vz-rosetta";
+        };
+      }
+    ];
+
+    system = linuxSystem;
+  }
+)
